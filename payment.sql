f2c9f5833b7d5e7d80c1e89f890d3d6cf9eaab59
Friemorn <dewondofriemorn.s4a@gmail.com>
2020-09-25 23:28:48 +0700

    add sql & postman

--- payment.sql
+++ payment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2020 at 08:53 AM
+-- Generation Time: Sep 25, 2020 at 06:27 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -38,7 +38,14 @@ CREATE TABLE `pin` (
 --
 
 INSERT INTO `pin` (`pinId`, `pin`, `userId`) VALUES
-(1, 123456, 1);
+(1, 111111, 1),
+(2, 123456, 2),
+(3, 333333, 3),
+(4, 444444, 4),
+(5, 555555, 5),
+(6, 666666, 6),
+(7, 777777, 7),
+(8, 888888, 8);
 
 -- --------------------------------------------------------
 
@@ -55,6 +62,15 @@ CREATE TABLE `transaction` (
   `notes` varchar(512) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`transactionId`, `senderId`, `receiverId`, `amount`, `createdAt`, `notes`) VALUES
+(1, 1, 2, 100000, '2020-09-25 16:09:47', 'Buat Kondangan'),
+(2, 2, 3, 200000, '2020-09-25 16:11:36', 'Bayar Utang'),
+(3, 7, 8, 2500000, '2020-09-25 16:13:44', 'Beli Perfect Grade Gundam');
+
 -- --------------------------------------------------------
 
 --
@@ -69,7 +85,7 @@ CREATE TABLE `user` (
   `email` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL,
   `phone` varchar(32) NOT NULL,
-  `image` varchar(128) NOT NULL,
+  `image` varchar(512) NOT NULL,
   `balance` int(11) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -79,7 +95,14 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userId`, `username`, `firstName`, `lastName`, `email`, `password`, `phone`, `image`, `balance`, `createdAt`) VALUES
-(1, 'betatester', 'John', 'Smith', 'betatest@gmail.com', '$2a$10$XKnq/aAcMNAFgnCBJHbBzOWkqsqroQg4wTZYrEl20cY6S65knV7xS', '0123456789', 'https://www.iconfinder.com/data/icons/ionicons/512/icon-ios7-contact-512.png', 0, '2020-09-25 06:38:21');
+(1, 'soloplayer', 'John', 'Smith', 'soloplayer@gmail.com', '$2a$10$5ykbYimPSraVwAPZDjiMv.k2o1vjnofw2BHjypbuWOzFUaAai4YSm', '0123456789', 'https://github.com/Friemorn/BackEnd-PaymentApp-ZWallet/blob/master/www.freepik.comfree-iconmale-user-shadow_751026.htm%23page=1&query=user&position=2.png?raw=true', 200000, '2020-09-25 16:08:52'),
+(2, 'dewo', 'Dewondo', 'Friemorn', 'df7@gmail.com', '$2a$10$FXtDLIZRjrcAGGj2XfBP..ntU/2TIivYJ60dLxWJhx2RWgcTewwcq', '08123456789', 'http://localhost:4000/api/v1/uploads/2020-09-25T15_57_30.298Ztibo.jpg', 100000, '2020-09-25 16:08:46'),
+(3, 'alan', 'John', 'Smith', 'alan@gmail.com', '$2a$10$ivM46XQtjQerEzE.m5u9p.lMJOoHLiIU04UoMKfVIiBuN7a4S/tWS', '0123456789', 'https://github.com/Friemorn/BackEnd-PaymentApp-ZWallet/blob/master/www.freepik.comfree-iconmale-user-shadow_751026.htm%23page=1&query=user&position=2.png?raw=true', 500000, '2020-09-25 16:10:19'),
+(4, 'erlangga', 'John', 'Smith', 'erlangga@gmail.com', '$2a$10$So6ij.pe/PtAsNt7OJFjd.4OhrD91I9mDoYw5FflTJ.Ke1plRiwR2', '0123456789', 'https://github.com/Friemorn/BackEnd-PaymentApp-ZWallet/blob/master/www.freepik.comfree-iconmale-user-shadow_751026.htm%23page=1&query=user&position=2.png?raw=true', 200000, '2020-09-25 16:10:30'),
+(5, 'kusuma', 'John', 'Smith', 'kusuma@gmail.com', '$2a$10$zwRx1N/pBaKba2K7Anr/L.EQmuYtzeao913gGw1IORexdCPT8jeO6', '0123456789', 'https://github.com/Friemorn/BackEnd-PaymentApp-ZWallet/blob/master/www.freepik.comfree-iconmale-user-shadow_751026.htm%23page=1&query=user&position=2.png?raw=true', 700000, '2020-09-25 16:12:56'),
+(6, 'ardiyanto', 'John', 'Smith', 'ardiyanto@gmail.com', '$2a$10$N8X6ke.fCgmIRq6jVF86bOb254iC0FokcX0FHHrnj7jlNNqx/AJ0e', '0123456789', 'https://github.com/Friemorn/BackEnd-PaymentApp-ZWallet/blob/master/www.freepik.comfree-iconmale-user-shadow_751026.htm%23page=1&query=user&position=2.png?raw=true', 500000, '2020-09-25 16:12:52'),
+(7, 'dewondo', 'John', 'Smith', 'dewondo@gmail.com', '$2a$10$WhlB6xFAsIfetPqeI4QtGOAoP6lCrbggc0tEuLS0f0BDaVuCRSP9K', '0123456789', 'https://github.com/Friemorn/BackEnd-PaymentApp-ZWallet/blob/master/www.freepik.comfree-iconmale-user-shadow_751026.htm%23page=1&query=user&position=2.png?raw=true', 5000000, '2020-09-25 16:13:03'),
+(8, 'friemorn', 'John', 'Smith', 'friemorn@gmail.com', '$2a$10$uWoZHIj89TjqY.ajvLR79uAyfzOc/xf4KmlE7rW9C4enxRM2HGtxW', '0123456789', 'https://github.com/Friemorn/BackEnd-PaymentApp-ZWallet/blob/master/www.freepik.comfree-iconmale-user-shadow_751026.htm%23page=1&query=user&position=2.png?raw=true', 2500000, '2020-09-25 16:13:08');
 
 --
 -- Indexes for dumped tables
@@ -90,14 +113,14 @@ INSERT INTO `user` (`userId`, `username`, `firstName`, `lastName`, `email`, `pas
 --
 ALTER TABLE `pin`
   ADD PRIMARY KEY (`pinId`),
-  ADD KEY `userId` (`userId`);
+  ADD KEY `pin_ibfk_1` (`userId`);
 
 --
 -- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`transactionId`),
-  ADD KEY `senderId` (`senderId`);
+  ADD KEY `transaction_ibfk_1` (`senderId`);
 
 --
 -- Indexes for table `user`
@@ -113,19 +136,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `pin`
 --
 ALTER TABLE `pin`
-  MODIFY `pinId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pinId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -135,13 +158,13 @@ ALTER TABLE `user`
 -- Constraints for table `pin`
 --
 ALTER TABLE `pin`
-  ADD CONSTRAINT `pin_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
+  ADD CONSTRAINT `pin_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
-  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`senderId`) REFERENCES `user` (`userId`);
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`senderId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
